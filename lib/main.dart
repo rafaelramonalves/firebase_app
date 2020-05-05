6ba943df2c983df10bd27f24e0b64fc63f5490dd
Rafael Ramon <rafael-kadu-ramon@hotmail.com>
2020-05-05 18:13:12 -0300

    Adicionando o upload de imagens(da camera ou galeria) para o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'; //Autenticação firebase
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+
+/*
 
 void main() async {
 
@@ -183,3 +188,106 @@ class  App extends StatelessWidget {
     return Container();
   }
 }
+]
+
+ */
+
+void main() async{
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+
+}
+
+class _HomeState extends State<Home> {
+
+
+
+  File imagem; //Imagem que será carregada
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Selecionar imagem"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+
+            RaisedButton(
+              child: Text("Camera"),
+              onPressed: (){
+                _recuparImagem(true);
+              },
+            ),
+
+            RaisedButton(
+              child: Text("Galeria"),
+              onPressed: (){
+                _recuparImagem(false);
+              },
+            ),
+            imagem == null ? //Condicional
+            Container()
+                : Image.file((imagem)),
+
+
+            RaisedButton(
+              child: Text("Upload Firebase"),
+              onPressed: (){
+
+                uploadImagem();
+              },
+            ),
+          ],
+
+        ),
+      ),
+    );
+  }
+
+  Future _recuparImagem(bool daCamera) async{
+
+    File imagemSelecionada;
+
+    if(daCamera){ //Camera
+      imagemSelecionada = await  ImagePicker.pickImage(
+          source: ImageSource.camera);
+    }else{ //Galeria
+      imagemSelecionada = await ImagePicker.pickImage(
+          source: ImageSource.gallery);
+    }
+
+    setState(() {
+      imagem = imagemSelecionada;
+    });
+  }
+
+  Future uploadImagem() async{
+
+
+    //Referenciar arquivo
+    FirebaseStorage storage = FirebaseStorage.instance;//pegar instância
+    StorageReference pastaRaiz = storage.ref(); //pegar referencia da pasta raiz
+    StorageReference arquivo = pastaRaiz.child("fotos").child("foto1.jpg"); //Criar pasta ou arquivo
+    // ex: "fotos" = pasta, "cao.png" = arquivo, "video.mov" = arquivo
+
+    //Fazer upload da imagem
+    arquivo.putFile(imagem);
+  }
+
+
+}
+
+
+
